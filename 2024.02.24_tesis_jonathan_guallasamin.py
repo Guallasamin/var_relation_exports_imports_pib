e3a558887990793d1b25af8884d0ad29f89ad097
Jonathan Guallasamin <jguallasamin@iMac-de-Jonathan.local>
2024-02-25 20:58:02 -0500

    Actualización 25 de febrero

--- 2024.02.24_tesis_jonathan_guallasamin.py
+++ 2024.02.24_tesis_jonathan_guallasamin.py
@@ -1,4 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
+# 1. Importar datos
+# 1.1. Importar datos de PIB trimestral
 pib_trimestral = pd.read_excel('/Users/jguallasamin/Desktop/Tesis/data_1708813067.xlsx', sheet_name='datos', decimal='.', index_col=0, usecols='A:G')
 print(pib_trimestral.head())
+
+# 1.2 Definir como serie de tiempo y graficar
+pib_trimestral.index = pd.to_datetime(pib_trimestral.index)
+
+
+
